6d4a14a55a4e78bd192023e3b48a91f565dc8bfd
JFernandoLe <leonmedellinjuanfernando@gmail.com>
2025-03-17 03:00:56 -0600

    Implementación del Control

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,62 @@
+import java.io.*;
+import java.lang.management.OperatingSystemMXBean;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Cliente {
+    public static void main(String[] args){
+        // Secuencias para cambiar colores
+        String rojo = "\u001B[31m";
+        String verde = "\u001B[32m";
+        String azul = "\u001B[34m";
+        String reset = "\u001B[0m";
+        //Scanner de Java
+        Scanner scanner = new Scanner(System.in);
+        try{
+            String dir="127.0.0.1"; //Direccion del servidor
+            int puerto=21;//Puerto de control para FTP
+            Socket c1=new Socket(dir,puerto);   //Hacemos la conexion al Socket
+            BufferedReader reader=new BufferedReader(new InputStreamReader(c1.getInputStream(),"ISO-8859-1"));
+            PrintWriter writer=new PrintWriter(new OutputStreamWriter(c1.getOutputStream(),"ISO-8859-1"));
+            //Leemos la bienvenida del servidor
+
+            System.out.println(azul+reader.readLine()+reset);
+            System.out.println(azul+"Escribe un comando: "+reset);
+            //Control para Ingresar y enviar comandos
+            while (true) {
+                //Leemos la entrada del comando
+                String comando= scanner.nextLine();
+                if(comando.compareToIgnoreCase("HELP")==0){
+                    //Comandos
+                    System.out.println(azul + "PWD" + reset + " - Mostrar directorio actual");//
+                    System.out.println(azul + "LS" + reset + " - Listar archivos y carpetas");//
+                    System.out.println(azul + "MKDIR" + reset + " - Crea el directorio indicado de forma remota");//
+                    System.out.println(azul + "CD <directorio>" + reset + " - Cambiar de directorio");
+                    System.out.println(azul + "GET <archivo>" + reset + " - Descargar un archivo");
+                    System.out.println(azul + "MGET <archivos>" + reset + " - Descargar multiples archivos");
+                    System.out.println(azul + "PUT <archivo>" + reset + " - Subir un archivo");
+                    System.out.println(azul + "MPUT <archivos>" + reset + " - Subir multiples archivos");
+                    System.out.println(azul + "DELETE <archivo>" + reset + " - Eliminar un archivo");//
+                    System.out.println(azul + "RENAME <archivo>" + reset + " - Cambia el nombre a un archivo");
+                    System.out.println(azul + "QUIT" + reset + " - Cerrar sesión");//
+                }
+                writer.println(comando); //Enviar comando al servidor
+                writer.flush(); //Se envia de inmediato
+
+                if(comando.compareToIgnoreCase("QUIT")==0){
+                    //Cerramos la conexion
+                    System.out.println("Cerrando sesion...");
+                    writer.close();
+                    reader.close();
+                    c1.close();
+                    System.exit(0);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,2 +1,98 @@
+import java.io.*;
+import java.net.*;
+import java.sql.SQLOutput;
+
 public class Servidor {
+    public static void main(String[] args){
+        try{
+            //Creamos el Socket de control
+            ServerSocket s1=new ServerSocket(21); //Asignamos el puerto 21, que es el de control para FTP
+            s1.setOption(StandardSocketOptions.SO_REUSEADDR,true);
+            System.out.println("Servidor iniciado en el puerto "+s1.getLocalPort());
+            //Creacion de la carpeta del usuario
+            File f=new File("");
+            String ruta=f.getAbsolutePath();
+            String carpeta="drive";
+            String rutaLocal=carpeta+"\\";
+            String ruta_archivos=ruta+"\\"+rutaLocal;
+            File f2=new File(ruta_archivos);
+            f2.mkdirs();
+            f2.setWritable(true);
+            for(;;){
+                Socket c1=s1.accept();
+                System.out.println("Cliente conectado desde "+c1.getInetAddress()+":"+c1.getPort());
+                BufferedReader reader=new BufferedReader(new InputStreamReader(c1.getInputStream(),"ISO-8859-1"));
+                PrintWriter writer=new PrintWriter(new OutputStreamWriter(c1.getOutputStream(),"ISO-8859-1"));
+                writer.println("Bienvenido a EscromDrive");
+                writer.flush();
+                //Creamos el Socket de datos
+                ServerSocket s2=new ServerSocket(20);
+                //Ciclo para recibir comandos
+                while(true){
+                    String comando=reader.readLine();
+                    if(comando.compareToIgnoreCase("QUIT")==0){
+                        System.out.println("Cliente cierra la conexion");
+                        reader.close();
+                        writer.close();
+                        c1.close();
+                        break;
+                    }else{
+                        System.out.println("Comando recibido: ");
+                        System.out.println(comando);
+                        // Responder a los comandos del cliente
+                        if (comando.startsWith("LS")) {
+                            //Obtenemos el Array de files
+                            File [] ficheros = f2.listFiles();
+                            //Iteramos sobre el Array
+                            for (File fs: ficheros) {
+                                if (fs.isFile()) {
+                                    System.out.println(fs.getName() + " es un fichero");
+                                }
+                                if (fs.isDirectory()) {
+                                    System.out.println(fs.getName() + " es un directorio");
+                                }
+                            }
+                        } else if (comando.startsWith("PWD")) {
+                            String texto=ruta_archivos; //Obtenemos la ruta general
+                            String dirAbs=texto.replace(f.getAbsolutePath(),""); //Conservamos unicamente la direccion de la carpeta Drive
+                            System.out.println(dirAbs);
+                        } else if (comando.startsWith("MKDIR")) {
+                            String texto = comando;
+                            String directorio = texto.replace("MKDIR ", ""); // Elimina el comando para tener unicamente el nombre
+                            //Creacion del directorio
+                            rutaLocal=directorio+"\\";
+                            ruta_archivos=ruta_archivos+rutaLocal;
+                            System.out.println(ruta_archivos);
+                            f2=new File(ruta_archivos);
+                            f2.mkdirs();
+                            f2.setWritable(true);
+                        } else if (comando.startsWith("DELETE")) {
+                            String texto = comando;
+                            String directorio = texto.replace("DELETE ", ""); // Elimina el comando para tener unicamente el nombre
+                            //Instanciamos la clase file con la ruta del fichero
+                            File miFichero = new File(ruta_archivos + directorio);
+                            //Comprobamos si existe el fichero
+                            if (miFichero.exists()) {
+                                //Borramos el fichero
+                                miFichero.delete();
+                            } else {
+                                System.out.println(miFichero.getName() + " NO existe");
+                            }
+                        } else if(comando.startsWith("CD")) {
+                            String texto=comando;
+                            String nuevaRuta=texto.replace("CD ",""); // Elimina el comando para tener unicamente la direccion
+                            ruta_archivos=f.getAbsolutePath()+nuevaRuta+"\\";
+                        } else if (comando.startsWith("QUIT")) {
+
+                            break;
+                        } else {
+                            System.out.println("502 Comando no implementado.");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
